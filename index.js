52b1dd4507fc675b24373f1608a4b2de55640749
Mohamed Emara <medoemara104@gmail.com>
2024-02-23 17:09:42 +0200

    Practice hot-relaod

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 
 app.get('/', (req, res) => {
-    res.send('<h1> Hello, World! </h1>');
+    res.send('<h1> He  llo, World! (Edited two) </h1>');
 })
 
 const PORT = 4000;
