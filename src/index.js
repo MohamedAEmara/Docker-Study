a6e2ff76710c3a6ed7e39a84e3fafbce291b77f6
Mohamed Emara <medoemara104@gmail.com>
2024-02-26 00:08:47 +0200

    Create mongo container & connect to

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+// Init app
 const app = express();
 
+
+// Connect DB
+const DB_USER = 'root'
+const DB_PASSWORD = 'example'
+const DB_PORT = 27017;          // Default for mongoDB
+const DB_HOST = '172.20.0.2'    // From docker inspect
+const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
+mongoose
+    .connect(URI)
+    .then(() => console.log('Connected to DB...'))
+    .catch(err => console.log('Failed to connect to DB ' + err));
 
 app.get('/', (req, res) => {
     res.send('<h1> Another line </h1>');
