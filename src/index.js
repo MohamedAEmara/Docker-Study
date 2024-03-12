30029f27c903ba8605baca70d9be92eeb6db3387
Mohamed Emara <medoemara104@gmail.com>
2024-03-12 17:46:29 +0200

    Initize basic setup for redis with docker-compose

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const redis = require('redis');
 
 // Init app
 const app = express();
 
+
+// Connect to Redis
+const redisClient = redis.createClient();
+redisClient.on('error', (err) => console.log('Redis Client Error:', err));
+redisClient.on('connect', () => console.log('Connected to redis...'));
+redisClient.connect();
+console.log('Test Test');
 
 // Connect DB
 const DB_USER = 'root'
@@ -13,14 +21,15 @@ const DB_HOST = 'mongo'    // From docker inspect
 const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
 mongoose
     .connect(URI)
-    .then(() => console.log('Connected to DB...'))
+    .then(() => console.log('Connected to DB........'))
     .catch(err => console.log('Failed to connect to DB ' + err));
+    
+    app.get('/', (req, res) => {
+        res.send('<h1> Another line </h1>');
+    })
 
-app.get('/', (req, res) => {
-    res.send('<h1> Another line </h1>');
-})
-
-const PORT = 4000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}...`);
+    const PORT = 4000;
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}...`);
+        console.log('Test Test Test');
 })
