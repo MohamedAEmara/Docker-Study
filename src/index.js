f302180fe5cff031339994dd9da68a6811b34033
Mohamed Emara <medoemara104@gmail.com>
2024-03-16 00:35:33 +0200

    Use postgres with docker

--- src/index.js
+++ src/index.js
@@ -21,27 +21,45 @@ redisClient.connect();
 // Connect DB
 const DB_USER = 'root'
 const DB_PASSWORD = 'example'
-const DB_PORT = 27017;          // Default for mongoDB
-const DB_HOST = 'mongo'    // From docker inspect
-const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
-mongoose
-    .connect(URI)
-    .then(() => console.log('Connected to DB........'))
-    .catch(err => console.log('Failed to connect to DB ' + err));
+const DB_PORT = 5432;           // Default for postgres
+const DB_HOST = 'postgres'      
+// const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
+// mongoose
+//     .connect(URI)
+//     .then(() => console.log('Connected to DB........'))
+//     .catch(err => console.log('Failed to connect to DB ' + err));
     
-    app.get('/', (req, res) => {
-        // Cash this key with this value in memory when user request this route..
-        redisClient.set('products', 'products...');
-        res.send('<h1> Another line </h1>');
-    })
+
+// Connect to Postgres
+const { Client} = require('pg');
+const connectionString = `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
+
+const client = new Client({
+    connectionString: connectionString,
+});
+
+console.log(client);
+
+client
+    .connect()
+    .then(() => console.log('Connected to PostgreSQL...'))
+    .catch((err) => console.log('Failed to connect to PostgreSQL...', err));
 
 
-    app.get('/data', async (req, res) => {
-        // Get the cashed products..
-        const products = await redisClient.get('products');
-        res.send(`<h1>${products}</h1>`)
-    })
-    const PORT = 4000;
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}...`);
+
+app.get('/', (req, res) => {
+    // Cash this key with this value in memory when user request this route..
+    redisClient.set('products', 'products...');
+    res.send('<h1> Another line </h1>');
+})
+
+
+app.get('/data', async (req, res) => {
+    // Get the cashed products..
+    const products = await redisClient.get('products');
+    res.send(`<h1>${products}</h1>`)
+})
+const PORT = 4000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}...`);
 })
